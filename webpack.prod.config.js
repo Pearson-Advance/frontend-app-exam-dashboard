3e393fcb9c8f6a06ac1859240eab12a99d3af603
Aura Alba <aura.alba@edunext.co>
2025-08-25 12:28:15 -0500

    feat: fix test errors

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const { createConfig } = require('@edx/frontend-build');
 
-module.exports = createConfig('webpack-dev', {
+module.exports = createConfig('webpack-prod', {
   resolve: {
     alias: {
       components: path.resolve(__dirname, 'src/components'),
       features: path.resolve(__dirname, 'src/features'),
       assets: path.resolve(__dirname, 'src/assets'),
     },
+    extensions: ['.js', '.jsx'],
   },
 });
