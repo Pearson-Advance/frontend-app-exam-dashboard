d575602890a0e55c2c69d32c7d5fb1467c4d008a
Nicolas Jimenez <a10100105555@gmail.com>
2025-09-19 10:36:28 -0500

    fix: footer styles

--- src/features/Main/index.jsx
+++ src/features/Main/index.jsx
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom';
 import { getConfig } from '@edx/frontend-platform';
 import Footer from '@edx/frontend-component-footer';
+import Header from '@edx/frontend-component-header';
 
 import SchedulePage from 'features/SchedulePage';
 import DashboardPage from 'features/DashboardPage';
 
 const Main = () => (
   <BrowserRouter basename={getConfig().EXAM_DASHBOARD_PATH}>
+    <Header />
     <Switch>
       <Route path="/exam" component={SchedulePage} />
       <Route path="/dashboard" component={DashboardPage} />
